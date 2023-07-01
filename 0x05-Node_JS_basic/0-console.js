8162afa5799aa40eee4eb5ac8a7c4212fea936f9
khendimike <52976404+khendimike@users.noreply.github.com>
2023-07-01 21:32:36 +0300

    Delete 0x05-Node_JS_basic directory

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,9 +0,0 @@
-/**
- * A function which display a message to the STDOUT
- * @param {String} message the message to be display
- */
-const displayMessage = (message) => {
-  console.log(message);
-};
-
-module.exports = displayMessage;
