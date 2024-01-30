c8848dbd03019cd6d78f8d6e8c4af61721cba83d
khendimike <52976404+khendimike@users.noreply.github.com>
2024-01-30 17:27:55 +0300

    Node js basics

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+/**
+ * isplay the message Welcome to Holberton School, what is your name?
+ * (followed by a new line)
+ * @author Yidnekachew Bantrga <https://github.com/Yidne21>
+ */
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("readable", () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
